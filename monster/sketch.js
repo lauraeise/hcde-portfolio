7e3da3f3dd7121910f5ded296ff2ec51f613d37d
laura.m.eise@gmail.com <laura.eise@hpe.com>
2017-02-26 12:03:05 -0800

    added Week 3 assignment

---
+++ monster/sketch.js
@@ -0,0 +1,6 @@
+// Created by Laura Eise
+// Last edited on 2/24/2017
+// Week 3 Assignment: Data Visualization
+// Learning Objectives: Practice using loops, arrays, functions, and parameters
+// Purpose: Provide an overview of the Common Vulnerabilities and Exploitations (CVE) of Micro Focus and HPE in preparation for the spin-merge in August 2017
+
